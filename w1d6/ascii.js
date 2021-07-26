8457b718aa5e35ea914ecab3fc5013c22bbf31e1
Yafiet Weldegabir <yafietweldegabir@gmail.com>
2021-07-25 23:24:30 -0500

    master asciimation11

---
+++ w1d6/ascii.js
@@ -0,0 +1,61 @@
+window.onload = function () {
+    textArea = document.getElementById("text-area");
+    animationSelected = document.getElementById("animation");
+    fontSize = document.getElementById("fontsize");
+    turboSpeed = document.getElementById("turbo");
+    startButton = document.getElementById("start");
+    stopButton = document.getElementById("stop");
+    
+    startButton.onclick = start;
+    stopButton.onclick = stop;
+    animationSelected.onchange = getFrames;
+    fontSize.onchange = setSize;
+    turboSpeed.onchange = turbo;
+
+}
+"use strict";
+var textArea, animationSelected, fontSize, timer, startButton, stopButton, speed = 250, turboSpeed;
+
+function start() {
+    var frames = textArea.value.split("=====\n");
+    var i = 0, l = frames.length;
+    (function iterator() {
+        textArea.value = frames[i];
+
+        if (++i >= l) {
+            i = 0;
+        }
+        timer = setTimeout(iterator, speed);
+    })();
+    stopButton.disabled = false;
+    startButton.disabled = true;
+};
+
+function stop(time) {
+    stopButton.disabled = true;
+    startButton.disabled = false;
+    clearTimeout(timer);
+    getFrames();
+}
+
+
+function getFrames() {
+    var dropSel = animationSelected.options[animationSelected.selectedIndex].innerHTML;
+    textArea.value = ANIMATIONS[dropSel];
+
+}
+
+function setSize() {
+    var fontsize = fontSize.options[fontSize.selectedIndex].value;
+    textArea.style.fontSize = fontsize;
+}
+
+function turbo() {
+    if (turboSpeed.checked) {
+        speed = 50;
+    }
+    else {
+        speed = 250;
+    }
+}
+
