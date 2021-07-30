e8f88861cee004fd07293b1db27db1d7bac8c518
Yafiet Weldegabir <yafietweldegabir@gmail.com>
2021-07-30 01:43:20 -0500

    master 3

--- w2d4/code.js
+++ w2d4/code.js
@@ -1,5 +1,37 @@
-String.prototype.filter= function(...bannedWords) {
-    return this.replace(new RegExp(bannedWords.join("|"), "gi"),'').replace(/\s+/g,' ').trim();
+String.prototype.filter = function (...bannedWords) {
+    return this.replace(new RegExp(bannedWords.join("|"), "gi"), '').replace(/\s+/g, ' ').trim();
 }
 
+Array.prototype.bubbleSort = function () {
+    let len = this.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (this[j] > this[j + 1]) {
+                let tmp = this[j];
+                this[j] = this[j + 1];
+                this[j + 1] = tmp;
+            }
+        }
+    }
+    return this;
+};
 
+/*3*/
+var Person = function () { };
+Person.prototype.initialize = function (name, age) {
+    this.name = name;
+    this.age = age;
+}
+Person.prototype.describe = function () {
+    return this.name + ", " + this.age + ", years old";
+}
+
+var Teacher = function () { };
+Teacher.prototype = new Person();
+Teacher.prototype.teach = function (subject) {
+    console.log(this.name + " is teaching " + subject);
+}
+
+var teacher1 = new Teacher();
+teacher1.initialize("Yafiet", 24);
+teacher1.teach("WaP");
