d1b231ca3605d30cfe54f3dadde0ab9ffaf818d2
Yafiet Weldegabir <yafietweldegabir@gmail.com>
2021-07-30 00:59:57 -0500

    master code

--- w2d4/code.js
+++ w2d4/code.js
@@ -1,5 +1,5 @@
 String.prototype.filter= function(...bannedWords) {
-    return this.replace(new RegExp(bannedWords.join("|"), "gi"),'');
+    return this.replace(new RegExp(bannedWords.join("|"), "gi"),'').replace(/\s+/g,' ').trim();
 }
 
 
