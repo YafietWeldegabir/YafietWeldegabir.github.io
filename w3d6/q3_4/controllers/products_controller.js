1013f9406182f950c6b61ab4261bbcdb4bba3c1f
Yafiet Weldegabir <yafietweldegabir@gmail.com>
2021-08-09 12:01:55 -0500

    master q34

--- w3d6/q3_4/controllers/products_controller.js
+++
@@ -1,9 +0,0 @@
-const Product = require("../models/product");
-
-exports.getProduct = (req, res) => {
-    res.render('product', Product.getProduct(req.params.id));
-};
-
-exports.addProduct = (req, res) => {
-    res.render('add-product', res.params);
-};
--- w3d6/q3_4/index.js
+++ w3d6/q3_4/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = require("./router");
 const path = require('path');
 const app = express();
 
@@ -7,8 +6,41 @@ app.set('view engine', 'ejs');
 
 app.set('views', path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/products', router);
+const PRODUCTS = [
+    { id: 1, name: 'item1', description: 'desc1', price: 1 },
+    { id: 2, name: 'item2', description: 'desc2', price:2 },
+    { id: 3, name: 'item3', description: 'desc3', price: 3 },
+    { id:4, name: 'item4', description:'desc4', price: 4 },
+    {id:5, name: 'item5', description:'desc5', price: 5}
+]
+
+ const CART=[
+    { name: 'item1', price: 1, quantity: 2 },
+    { name: 'item2', price: 2, quantity: 4 }];
+var prodIndex;
+app.get('/product/:id', (req, res) => {
+     prodIndex= PRODUCTS.findIndex(pr=>pr.id===parseInt(req.params.id));
+     if(prodIndex<0){
+        res.send("The requested product is not available");
+     }
+     else{
+        res.render("product", {
+            id:PRODUCTS[prodIndex].id,
+            name:PRODUCTS[prodIndex].name,
+            description:PRODUCTS[prodIndex].description,
+            price:PRODUCTS[prodIndex].price
+            });
+     }
+});
+
+app.post('/addToCart', (req, res) => {
+    //dynamically adding products to cart
+    CART.push(PRODUCTS.find(e => e.id === parseInt(req.body.id)));
+    res.render("shoppingcart", {
+        products: CART
+    });
+});
 
 app.listen(3000);
